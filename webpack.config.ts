58ecc0185e4e2bdce8da6bd83d7a0a9f59562d5f
Trevor Von Seggern <tvoman2@gmail.com>
2017-05-06 18:14:14 -0600

    Removed unnecessary imports.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,5 @@
-import * as webpack from 'webpack';
 import * as path from 'path';
 import * as process from 'process';
-import * as fs from 'fs';
 
 let entry = path.join(process.cwd(), process.env.SERVER_TARGET || 'client');
 
